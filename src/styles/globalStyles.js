13f30cbea118a528e889a29f6152dd0801421a35
Guibson Arcebispo <guibsonarc@gmail.com>
2020-10-11 15:41:35 -0300

    chore: clean project structure

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,15 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
-    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    height: 100%;
+    width: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
     font-family: 'Archivo', sans-serif;
   }
 
-  body {
+  html {
     -webkit-font-smoothing: antialiased;
     background: white
   }
