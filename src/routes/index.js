488414ca2142ac62cfc62af4f32ce81815554178
Guibson Arcebispo <guibsonarc@gmail.com>
2020-10-11 19:17:30 -0300

    fix: fix routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,23 +3,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import ProtectedRoute from 'routes/ProtectedRoute';
 import Home from 'pages/Home';
-<<<<<<< HEAD
-import Store from 'pages/Store';
-
-const Routes = () => (
-  <Switch>
-    <Route path="/" component={Home} exact />
-    <Route path="/store" component={Store} />
-=======
 import Login from 'pages/Login';
 import Register from 'pages/Register';
+import Store from 'pages/Store';
 
 const Routes = () => (
   <Switch>
     <ProtectedRoute exact path="/" component={Home} />
     <Route path="/register" component={Register} />
     <Route path="/login" component={Login} />
->>>>>>> f1b123cf39c1dd5a8b05e004b4a2d82939509ddd
+    <Route path="/store" component={Store} />
   </Switch>
 );
 
